2951efa3eebe01c889ef10e1827571f6a4caf992
jonathan justman <jojo.justman@gmail.com>
2021-11-08 23:04:13 +0100

    update user profile info in progress

--- src/components/Profile/MyProfile.js
+++ src/components/Profile/MyProfile.js
@@ -1,17 +1,37 @@
 import MyProfileForm from "./MyProfileForm";
 import classes from "./MyProfile.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
 
 const MyProfile = () => {
   const token = useSelector((state) => state.auth.token);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+  const [id, setId] = useState("");
   const [description, setDescription] = useState("");
+  const usernameInput = useRef();
 
   useEffect(() => {
     fetchMyProfile();
   }, []);
+
+  const updateUsername = async (e) => {
+    console.log(usernameInput.current.value);
+    e.preventDefault();
+    const dataAuth = {
+      username: usernameInput.current.value,
+    };
+    const data = await fetch(`http://localhost:1337/users/${id}`, {
+      method: "put",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataAuth),
+    });
+    const res = await data.json();
+    console.log(res);
+  };
 
   const fetchMyProfile = async () => {
     const data = await fetch("http://localhost:1337/users/me", {
@@ -20,10 +40,10 @@ const MyProfile = () => {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       },
-      // body: JSON.stringify(dataAuth),
     });
     const res = await data.json();
     console.log(res);
+    setId(res.id);
     setUsername(res.username);
     setEmail(res.email);
     setDescription(res.description);
@@ -38,6 +58,10 @@ const MyProfile = () => {
       <p>{email}</p>
       <h2>Votre description :</h2>
       {description && <p>{description}</p>}
+      <form onSubmit={updateUsername}>
+        <input placeholder="Nouvel username" type="text" ref={usernameInput} />
+        <button type="submit">Modifier !</button>
+      </form>
 
       <MyProfileForm />
     </section>
